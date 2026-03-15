98f912ca9b2182fde34dbb9647f72dd240e3f37a
Sergey_Mashinin <smashinin@mail.ru>
2026-03-15 14:08:51 +0300

    тесты для задания_1

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,61 +1,51 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        // Автоматическая загрузка подходящего chromedriver
+        WebDriverManager.chromedriver().setup();
+
+    }
+
     @BeforeEach
     void setUp() {
-        // Открываем страницу с формой
         open("http://localhost:9999");
-        Configuration.holdBrowserOpen = false; // можно оставить false для CI
+        Configuration.timeout = 15000;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void shouldSuccessfullySubmitForm() {
-        // Генерируем дату: текущая + 3 дня
-        LocalDate deliveryDate = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String plannedDate = deliveryDate.format(formatter);
+        LocalDate deliveryDate = LocalDate.now().plusDays(7);
+        String formattedDate = deliveryDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-        // Ввод города (например, "Москва")
         $("[data-test-id=city] input").setValue("Москва");
-
-        // Очистка поля даты (если там уже есть значение)
-        $("[data-test-id=date] input").doubleClick(); // выделяем текст двойным кликом
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE); // удаляем выделенное
-        $("[data-test-id=date] input").setValue(plannedDate);
-
-        // Ввод имени
-        $("[data-test-id=name] input").setValue("Иванов-Петров Иван");
-
-        // Ввод телефона
-        $("[data-test-id=phone] input").setValue("+71234567890");
-
-        // Отметка чекбокса согласия
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(formattedDate);
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
 
-        // Нажатие на кнопку "Забронировать"
-        $("[data-test-id=button] button").click();
-
-        // Ожидаем появления спиннера загрузки и его исчезновения (не более 15 секунд)
-        $("[data-test-id=loading]").shouldBe(Condition.visible, Duration.ofSeconds(5)); // спиннер появился
-        $("[data-test-id=loading]").shouldBe(Condition.hidden, Duration.ofSeconds(15)); // исчез
-
-        // Проверяем появление всплывающего окна с сообщением об успехе
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(5));
-        // Или более точный селектор, если есть data-test-id у окна
-        // $("[data-test-id=success-notification]").shouldBe(Condition.visible);
+        $("[data-test-id=notification]").should(appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content")
+                .shouldHave(text("Встреча успешно забронирована"));
     }
 }
