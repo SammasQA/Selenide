f5ac16fb4b96048336a9594434b8345f7e70622c
Sergey_Mashinin <smashinin@mail.ru>
2026-03-15 14:46:32 +0300

    add tests_2

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,12 +1,13 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -19,31 +20,49 @@ public class CardDeliveryTest {
 
     @BeforeAll
     static void setUpAll() {
-        // Автоматическая загрузка подходящего chromedriver
         WebDriverManager.chromedriver().setup();
-
+       // Configuration.timeout = 15000; //для тестов
     }
 
     @BeforeEach
     void setUp() {
+        Configuration.holdBrowserOpen = false; // для отладки
         open("http://localhost:9999");
-        Configuration.timeout = 15000;
-        Configuration.holdBrowserOpen = false; // для ci false
     }
 
     @Test
-    void shouldSuccessfullySubmitForm() {
-        LocalDate deliveryDate = LocalDate.now().plusDays(7);
-        String formattedDate = deliveryDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    void shouldSuccessfullySubmitFormWithComplexElements() {
+        // --- Город с выбором из выпадающего списка ---
+        $("[data-test-id=city] input").setValue("Мо");
+        $(".input__menu").should(appear, Duration.ofSeconds(10));
+        $$(".menu-item").findBy(text("Москва")).click();
 
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(formattedDate);
+        // --- Дата через календарь на неделю вперёд ---
+        // Открываем календарь (кнопка с иконкой рядом с полем даты)
+        $("[data-test-id=date] button").click();
+
+        // Ждём появления календаря (любая ячейка с классом calendar__day)
+        $(".calendar__day").should(appear, Duration.ofSeconds(5));
+
+        // Вычисляем нужную дату: текущая + 7 дней
+        LocalDate targetDate = LocalDate.now().plusDays(7);
+        int targetDay = targetDate.getDayOfMonth();
+
+
+        $$(".calendar__day").findBy(text(String.valueOf(targetDay))).click();
+
+        // Проверяем, что поле даты заполнилось корректно
+        String expectedDate = targetDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").shouldHave(value(expectedDate));
+        // --- Остальные поля ---
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79111111111");
         $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
 
+        // Отправка формы
+        $$("button").find(exactText("Забронировать")).shouldBe(enabled).click();
+
+        // Проверка уведомления
         $("[data-test-id=notification]").should(appear, Duration.ofSeconds(15));
         $("[data-test-id=notification] .notification__content")
                 .shouldHave(text("Встреча успешно забронирована"));
